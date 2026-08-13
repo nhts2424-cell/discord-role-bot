bb44a55701c5bee119ff5a766ddc00004b4ec8b5
nhts2424-cell <nhts2424@gmail.com>
2026-08-13 14:32:55 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,9 @@
+import os
 import discord
 from discord.ext import commands
 
 intents = discord.Intents.default()
-
-bot = commands.Bot(
-    command_prefix="!",
-    intents=intents
-)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
@@ -16,4 +13,4 @@ async def on_ready():
 async def test(ctx):
     await ctx.send("Bot đang hoạt động!")
 
-bot.run("YOUR_BOT_TOKEN")
+bot.run(os.getenv("DISCORD_TOKEN"))
